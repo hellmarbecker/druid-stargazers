2fcece7467b76c62d58573e88381192c89cb9082
hellmarbecker <becker@hellmar-becker.de>
2023-05-05 15:13:26 +0200

    Add loop, remove hardcoded token

--- druid-stargazers.py
+++ druid-stargazers.py
@@ -1,15 +1,39 @@
 import requests
+import json
+import os
+
+url = "https://api.github.com/repos/hellmarbecker/blog/stargazers"
+# url = "https://api.github.com/repos/apache/druid/stargazers"
+token = os.environ['GITHUB_TOKEN']
+headers = {
+    "Accept": "application/vnd.github+json",
+    "Authorization": "Bearer " + token,
+    "X-GitHub-Api-Version": "2022-11-28"
+}
+
+def get_all_stargazers():
+
+    params = { "per_page": 10, "page": 1 }
+    gazers = []
+
+    while True:
+        r = requests.get(url, headers=headers, params=params)
+        partList = r.json()
+        print(partList)
+        elements = len(partList)
+        print(params)
+        print(f'#elements: {elements}')
+        if elements == 0:
+            break
+        gazers += partList
+        params['page'] += 1
+    return gazers
 
 def main():
 
-    url = "https://api.github.com/repos/apache/druid/stargazers"
-    headers = {
-        "Accept": "application/vnd.github+json",
-        "Authorization": "Bearer 62f49ab49c0c2d32c40bacc81256e12dcd566dd1",
-        "X-GitHub-Api-Version": "2022-11-28"
-    }
-    r = requests.get(url, headers=headers)
-    print(r.json())
+    print("main")
+    l = get_all_stargazers()
+    print(l)
 
 if __name__ == "__main__":
     main()
