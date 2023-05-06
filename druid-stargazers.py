6352114a2379aa8ac3c882814e8a2e7a6c1586ac
hellmarbecker <becker@hellmar-becker.de>
2023-05-06 10:52:02 +0200

    Extract starred url

--- druid-stargazers.py
+++ druid-stargazers.py
@@ -1,28 +1,35 @@
 import requests
 import json
 import os
+import re
 
-url = "https://api.github.com/repos/hellmarbecker/blog/stargazers"
+repo_url = "https://api.github.com/repos/apache/druid-website-src/stargazers"
 # url = "https://api.github.com/repos/apache/druid/stargazers"
 token = os.environ['GITHUB_TOKEN']
 headers = {
-    "Accept": "application/vnd.github+json",
+    "Accept": "application/vnd.github.star+json",
     "Authorization": "Bearer " + token,
     "X-GitHub-Api-Version": "2022-11-28"
 }
 
-def get_all_stargazers():
+def get_all_starred(user_record):
+    url = user_record["user"]["starred_url"]
+    url = re.sub(r'\{/[^\}]+\}', '', url)
+    print(url)    
 
+def get_all_stargazers(owner, repo):
+
+    url = "https://api.github.com/repos/" + owner + "/" + repo + "/stargazers"
     params = { "per_page": 10, "page": 1 }
     gazers = []
 
     while True:
         r = requests.get(url, headers=headers, params=params)
         partList = r.json()
-        print(partList)
+        # print(partList)
         elements = len(partList)
-        print(params)
-        print(f'#elements: {elements}')
+        # print(params)
+        # print(f'#elements: {elements}')
         if elements == 0:
             break
         gazers += partList
@@ -31,11 +38,16 @@ def get_all_stargazers():
 
 def main():
 
-    print("main")
-    l = get_all_stargazers()
-    print(l)
+    # print("main")
+    owner = "apache"
+    repo = "druid-website-src"
+    l = get_all_stargazers(owner, repo)
+    # print(l)
     for u in l:
-        print u
+        u["starred_repo"] = owner + "/" + repo
+        print(json.dumps(u))
+    for u in l:
+        get_all_starred(u)
 
 if __name__ == "__main__":
     main()
