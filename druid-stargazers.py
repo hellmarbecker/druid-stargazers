6ea2805afe5d9a70de57989340ca9c5f85020398
hellmarbecker <becker@hellmar-becker.de>
2023-05-07 14:39:12 +0200

    Add error check

--- druid-stargazers.py
+++ druid-stargazers.py
@@ -2,34 +2,53 @@ import requests
 import json
 import os
 import re
+import logging
+import time
+
 
 TOKEN = os.environ['GITHUB_TOKEN']
 OWNER = "apache"
-REPO = "druid-website-src"
-# REPO = "druid"
+# REPO = "druid-website-src"
+REPO = "druid"
 
-def get_all_starred(user_record):
 
-    url = user_record["user"]["starred_url"]
-    url = re.sub(r'\{/[^\}]+\}', '', url)
+def get_paginated_list(url, headers):
+
+    params = { "per_page": 100, "page": 1 }
+    resultList = []
+    # print(f'-------- Getting paginated result for {url}')
+
+    while True:
+        backoff = 10
+        while True:
+            r = requests.get(url, headers=headers, params=params)
+            code = r.status_code
+            logging.info(f'-------- Page {params}: return code {code}')
+            if code == 200:
+                break
+            else:
+                logging.info(f'-------- Page {params}: sleeping {backoff} before retry')
+                time.sleep(backoff)
+                backoff *= 2
+        partList = r.json()
+        elements = len(partList)
+        logging.info(f'-------- Page {params}: Got {elements} elements')
+        if elements == 0:
+            break
+        resultList += partList
+        params['page'] += 1
+    return resultList
+
+
+def get_all_starred(st_url):
+
+    url = re.sub(r'\{/[^\}]+\}', '', st_url)
     headers = {
         "Accept": "application/vnd.github+json",
         "Authorization": "Bearer " + TOKEN,
         "X-GitHub-Api-Version": "2022-11-28"
     }
-    params = { "per_page": 100, "page": 1 }
-    starred = []
-    # print(f'-------- Getting stars for {url}')
-
-    while True:
-        r = requests.get(url, headers=headers, params=params)
-        partList = r.json()
-        elements = len(partList)
-        # print(f'-------- Page {params}: Got {elements} elements')
-        if elements == 0:
-            break
-        starred += partList
-        params['page'] += 1
+    starred = get_paginated_list(url, headers)
     starred_repos = [ x['full_name'] for x in starred ]
     # print(json.dumps(starred_repos))
     return starred_repos
@@ -43,27 +62,27 @@ def get_all_stargazers(owner, repo):
         "Authorization": "Bearer " + TOKEN,
         "X-GitHub-Api-Version": "2022-11-28"
     }
-    params = { "per_page": 100, "page": 1 }
-    gazers = []
-
-    while True:
-        r = requests.get(url, headers=headers, params=params)
-        partList = r.json()
-        elements = len(partList)
-        if elements == 0:
-            break
-        gazers += partList
-        params['page'] += 1
+    gazers = get_paginated_list(url, headers)
     return gazers
 
 
 def main():
 
+    logLevel = logging.INFO
+    logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', level=logLevel)
+
+    logging.info(f'Getting users for repo {OWNER}/{REPO}')
     l = get_all_stargazers(OWNER, REPO)
     # print(l)
+
+    # testlist = get_all_starred('https://api.github.com/users/chinnurtb/starred{/owner}{/repo}')
+    # print(json.dumps(testlist))
+    # l = []
+
     for u in l:
-        u["starred_repo"] = owner + "/" + repo
-        u["other_starred_repos"] = get_all_starred(u)
+        u["starred_repo"] = OWNER + "/" + REPO
+        logging.info(f'Getting stars for user {u["user"]["login"]}')
+        u["other_starred_repos"] = get_all_starred(u["user"]["starred_url"])
         print(json.dumps(u))
 
 
