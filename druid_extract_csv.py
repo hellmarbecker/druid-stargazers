2e4a2604457554bdf82b7821f94c631da7a5ca50
hellmarbecker <becker@hellmar-becker.de>
2023-05-15 19:50:54 +0000

    Remove newlines inside strings

--- druid_extract_csv.py
+++ druid_extract_csv.py
@@ -1,4 +1,5 @@
 import json
+import re
 import csv
 import logging
 import sys
@@ -12,8 +13,9 @@ def main():
 
     # Read stargazer records from stdin
     fieldnames = [ "starred_at", "email", "name", "login", "id", "twitter_username", "blog", "location" ] 
+    set_nested_fieldnames = set(fieldnames[1:])
 
-    writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
+    writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
     writer.writeheader()
 
     while True:
@@ -22,7 +24,11 @@ def main():
         if not next_line:
             break;
         rec = json.loads(next_line.strip())
-        csvrec = { key : rec["user"][key] for key in fieldnames[1:] }
+        csvrec = { key : rec["user"][key] for key in rec["user"].keys() & set_nested_fieldnames }
+        for key in csvrec:
+            if type(csvrec[key]) is str:
+                csvrec[key] = re.sub("\n", "", csvrec[key])
+
         csvrec["starred_at"] = rec["starred_at"]
         writer.writerow(csvrec)
 
