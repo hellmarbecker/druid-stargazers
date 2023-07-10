798338f16e92b7aa3b9ad6a8167239ba73e1040c
Hellmar Becker <becker@hellmar-becker.de>
2023-07-10 14:21:14 +0200

    Create cumulative-monthly-selfjoin.sql

---
+++ cumulative-monthly-selfjoin.sql
@@ -0,0 +1,13 @@
+WITH cte AS (
+  SELECT DATE_TRUNC('MONTH', "__time") AS date_month, starred_repo, COUNT(*) AS count_monthly
+  FROM "stargazers-ecosystem"
+  GROUP BY 1, 2
+)
+SELECT
+  cte.date_month,
+  cte.starred_repo,
+  SUM(t2.count_monthly) AS sum_cume
+FROM cte INNER JOIN cte t2 ON cte.starred_repo = t2.starred_repo
+WHERE t2.date_month <= cte.date_month
+GROUP BY 1, 2
+ORDER BY 1, 2
