962ccd8817d969600eb4267b4801ad4f455a0736
Hellmar Becker <becker@hellmar-becker.de>
2023-08-11 16:25:09 +0200

    Make sure to generate json lines format

--- extract_cohorts.sh
+++ extract_cohorts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-jq '[ ., inputs | { starred_at, login: .user.login, starred_repo } ] | group_by(.login) | [ .[] | { login: .[0].login, repos: [ .[] | .starred_repo ] | sort } ]' multi_repo_20230710-01.json
+ jq -c '[ ., inputs | { starred_at, login: .user.login, starred_repo } ] | group_by(.login) | [ .[] | { login: .[0].login, repos: [ .[] | .starred_repo ] | sort } ] | .[]' multi_repo_20230710-01.json
